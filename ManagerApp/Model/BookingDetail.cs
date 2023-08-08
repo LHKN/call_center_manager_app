746a87ea4135f1f873bab5ee373762e183de9956
LHKN <79393909+LHKN@users.noreply.github.com>
2023-08-08 15:32:19 +0700

    chore: trivial updates for models and existing layout

---
+++ ManagerApp/Model/BookingDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Model
+{
+    class BookingDetail
+    {
+        private int _id;
+        private int _customerPhone;
+        private int _customerRole;
+        //private int _customerId;
+
+        private DateOnly? _orderDate;
+        private int _price;
+        private int _rating;
+        private int _status;
+
+        //transport means
+        private int _driverId;
+        private int _transportType;
+
+        //locations
+        private string _pickupLocation;
+        private string _destination;
+
+        //datetime
+        private DateOnly? _departureDate;
+        // departureTime hh:mm:ss?
+
+
+        public int Id { get => _id; set => _id = value; }
+        public int CustomerPhone { get => _customerPhone; set => _customerPhone = value; }
+        public int CustomerRole { get => _customerRole; set => _customerRole = value; }
+        
+        
+        public DateOnly? DepartureDate { get => _departureDate; set => _departureDate = value; }
+    }
+}
---
+++ ManagerApp/Model/EWallet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+
+namespace ManagerApp.Model
+{
+    public class EWallet : INotifyPropertyChanged
+    {
+        protected int _id;
+        protected string _userId;
+        protected string _phoneNumber;
+        protected int _balance;
+
+        public int Id { get => _id; set => _id = value; }
+        public string UserId { get => _userId; set => _userId = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public int Balance { get => _balance; set => _balance = value; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
