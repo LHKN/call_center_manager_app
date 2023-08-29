14a0e0fbe25dc8904de196bfecae59762b8ff715
LHKN <79393909+LHKN@users.noreply.github.com>
2023-08-29 21:41:59 +0700

    fix: update models and booking page calendar

--- ManagerApp/Model/Account.cs
+++ ManagerApp/Model/Account.cs
@@ -10,19 +10,15 @@ namespace ManagerApp.Model
     public class Account : INotifyPropertyChanged
     {
         protected int _id;
-        //protected string _name;
-        //protected string _phoneNumber;
-        //private string address;
-        //protected string _username;
-        //protected string _password;
+        protected string _name;
+        protected string _username;
+        protected string _password;
         protected Role _role;
 
         public int Id { get => _id; set => _id = value; }
-        //public string Name { get => _name; set => _name = value; }
-        //public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
-        //public string Address { get => address; set => address = value; }
-        //public string Username { get => _username; set => _username = value; }
-        //public string Password { get => _password; set => _password = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
         public Role Role { get => _role; set => _role = value; }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ ManagerApp/Model/UserAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ManagerApp.Model
+{
+    public class UserAccount : Account
+    {
+        //protected string _phoneNumber;
+        //private string address;
+
+        //public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        //public string Address { get => address; set => address = value; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
