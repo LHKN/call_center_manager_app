ac6811d6f15727c9de2cb01b5b7531e84bc3251d
LHKN <79393909+LHKN@users.noreply.github.com>
2023-09-07 00:24:05 +0700

    feat: implete get all bookings data from firebase

--- ManagerApp/Repository/BookingRepository.cs
+++ ManagerApp/Repository/BookingRepository.cs
@@ -19,36 +19,65 @@ namespace ManagerApp.Repository
     {
         public bool SetBookingDetail(IFirebaseClient client, BookingDetail booking)
         {
-            SetResponse res = client.Set("Bookings/" + booking.Id + "/PhoneNumber", booking.PhoneNumber);
-            if (res.Body == null) return false;
+            try
+            {
+                SetResponse res = client.Set("Bookings/" + booking.Id + "/PhoneNumber", booking.PhoneNumber);
+                res = client.Set("Bookings/" + booking.Id + "/CustomerRole", booking.CustomerRole.ToString());
+                res = client.Set("Bookings/" + booking.Id + "/PickupLocationName", booking.PickupLocationName);
+                res = client.Set("Bookings/" + booking.Id + "/DestinationName", booking.DestinationName);
+                res = client.Set("Bookings/" + booking.Id + "/PickupTime", booking.PickupTime.ToString());
+                res = client.Set("Bookings/" + booking.Id + "/PickupDate", booking.PickupDate.ToString());
+                res = client.Set("Bookings/" + booking.Id + "/Price", booking.Price.ToString());
+                res = client.Set("Bookings/" + booking.Id + "/Rating", booking.Rating.ToString());
+                res = client.Set("Bookings/" + booking.Id + "/Transport", booking.Transport);
+                res = client.Set("Bookings/" + booking.Id + "/Status", booking.Status.ToString());
+            }
+            catch 
+            {
+                return false;
+            }
+            
+            return true;
+        }
 
-            res = client.Set("Bookings/" + booking.Id + "/CustomerRole", booking.CustomerRole.ToString());
-            if (res.Body == null) return false;
+        public bool GetBookingDetail(IFirebaseClient client, ref BookingDetail booking)
+        {
+            try
+            {
+                var res = client.Get("Bookings/" + booking.Id + "/PhoneNumber");
+                booking.PhoneNumber = res.ResultAs<string>();
 
-            res = client.Set("Bookings/" + booking.Id + "/PickupLocationName", booking.PickupLocationName);
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/CustomerRole");
+                booking.CustomerRole = int.Parse(res.ResultAs<string>());
 
-            res = client.Set("Bookings/" + booking.Id + "/DestinationName", booking.DestinationName);
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/PickupLocationName");
+                booking.PickupLocationName = res.ResultAs<string>();
 
-            res = client.Set("Bookings/" + booking.Id + "/PickupTime", booking.PickupTime.ToString());
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/DestinationName");
+                booking.DestinationName = res.ResultAs<string>();
 
-            res = client.Set("Bookings/" + booking.Id + "/PickupDate", booking.PickupDate.ToString());
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/PickupTime");
+                booking.PickupTime = TimeSpan.Parse(res.ResultAs<string>());
 
-            res = client.Set("Bookings/" + booking.Id + "/Price", booking.Price.ToString());
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/PickupDate");
+                booking.PickupDate = DateOnly.Parse(res.ResultAs<string>());
 
-            res = client.Set("Bookings/" + booking.Id + "/Rating", booking.Rating.ToString());
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/Price");
+                booking.Price = int.Parse(res.ResultAs<string>());
 
-            res = client.Set("Bookings/" + booking.Id + "/Transport", booking.Transport);
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/Rating");
+                booking.Rating = int.Parse(res.ResultAs<string>());
 
-            res = client.Set("Bookings/" + booking.Id + "/Status", booking.Status.ToString());
-            if (res.Body == null) return false;
+                res = client.Get("Bookings/" + booking.Id + "/Transport");
+                booking.Transport = res.ResultAs<string>();
 
+                res = client.Get("Bookings/" + booking.Id + "/Status");
+                booking.Status = int.Parse(res.ResultAs<string>());
+            }
+            catch
+            {
+                return false;
+            }
             return true;
         }
 
@@ -89,9 +118,50 @@ namespace ManagerApp.Repository
             throw new NotImplementedException();
         }
 
-        public Task<ObservableCollection<BookingDetail>> GetAll()
+        public async Task<ObservableCollection<BookingDetail>> GetAll()
         {
-            throw new NotImplementedException();
+            ObservableCollection<BookingDetail> bookings = new ObservableCollection<BookingDetail>();
+            IFirebaseClient client;
+
+            try
+            {
+                client = await TryConnect();
+            }
+            catch
+            {
+                await App.MainRoot.ShowDialog("Can not connect to Firebase", "There was a problem with your internet!");
+                return bookings;
+            }
+
+            var res = client.Get("Bookings/count");
+            if (res.Body == null) return bookings;
+            int count = int.Parse(res.ResultAs<string>());
+            if (count == 0) return bookings;
+
+            List<int> idList = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                res = client.Get("Bookings/idList/" + i);
+                if (res.Body == null) return bookings;
+                int temp = int.Parse(res.ResultAs<string>());
+                idList.Add(temp);
+            }
+
+            foreach (int id in idList)
+            {
+                BookingDetail temp = new BookingDetail();
+                temp.Id = id;
+                bool result = GetBookingDetail(client, ref temp);
+
+                if (!result) { 
+                    await App.MainRoot.ShowDialog("Error", "Unable to fetch data... Please check database entries");
+                    break;
+                }
+                bookings.Add(temp);
+            }
+
+            return bookings;
         }
 
         public Task<BookingDetail> GetById(int id)
