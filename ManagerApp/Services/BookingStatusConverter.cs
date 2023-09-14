9f090e86b2853d862e02aede2ad97c67b84ae6d6
LHKN <79393909+LHKN@users.noreply.github.com>
2023-09-14 10:41:17 +0700

    fix: update converters for numeral states

---
+++ ManagerApp/Services/BookingStatusConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Services
+{
+    class BookingStatusConverter : IValueConverter
+    {
+        static Dictionary<int, string> types = new Dictionary<int, string> {
+            { 0, "Inactive"},
+            { 1, "Active" }
+        };
+
+        public object Convert(object value, System.Type targetType, object parameter, string language)
+        {
+            int status = (int)value;
+            return types[status];
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, string language)
+        {
+            string typeString = (string)value;
+            return types.FirstOrDefault(x => x.Value.Equals(typeString)).Key;
+        }
+    }
+}
---
+++ ManagerApp/Services/CustomerRoleConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Services
+{
+    class CustomerRoleConverter : IValueConverter
+    {
+        static Dictionary<string, string> types = new Dictionary<string, string> {
+            { "0", "Standard"},
+            { "1", "VIP" },
+            { "2", "Not Using App" }
+        };
+
+        public object Convert(object value, System.Type targetType, object parameter, string language)
+        {
+            string customerType = (string)value;
+            return types[customerType];
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, string language)
+        {
+            string typeString = (string)value;
+            return types.FirstOrDefault(x => x.Value.Equals(typeString)).Key;
+        }
+    }
+}
